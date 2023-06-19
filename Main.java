0f0704139ff819279bea99829e7a1d897460a8f5
Henrique-hpds <h260497@dac.unicamp.br>
2023-06-19 00:58:08 -0300

    adição cores

---
+++ Main.java
@@ -0,0 +1,33 @@
+import javax.swing.*;  
+
+public class Main {  
+
+	static Icon casaBege = new ImageIcon("imagens/bege.png");
+	static Icon casaVerde = new ImageIcon("imagens/verde.png");
+	static Icon peaoBrancoCasaBranca = new ImageIcon("imagens/peaoBrancoCasaBranca.png");
+
+	public static JButton botao(Icon img, int x, int y, int largura, int altura){
+		JButton b = new JButton(img);  
+		b.setBounds(x,y,largura, altura);
+		return b;
+	}
+
+    public static void main(String[] args) {  
+
+		JFrame tela = new JFrame();
+
+		// inicia o tabuleiro vazio
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if((i + j) % 2 == 0)
+					tela.add(botao(casaBege, 100 * i, 100 * j ,100,100));
+				else
+					tela.add(botao(casaVerde, 100 * i, 100 * j ,100,100));
+			}
+		}
+		
+		tela.setSize(2000,2000);  
+		tela.setLayout(null);
+		tela.setVisible(true);  
+	}  
+}  
