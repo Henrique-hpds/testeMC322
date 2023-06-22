23e1234a1a9c59bbdd1d5cc55db4b169dfa0f1d3
Victor Satoh <47898144+vsatoh@users.noreply.github.com>
2023-06-22 17:06:40 -0300

    mudei alguns metodos e variaveis

--- src/Peca.java
+++ src/Peca.java
@@ -2,33 +2,24 @@ import java.util.ArrayList;
 
 abstract public class Peca {
     public final String cor;
-    public int posicaoX, posicaoY;
+    public int posicao;
 
-    public Peca(String cor, int posicaoX, int posicaoY) {
+    public Peca(String cor, int posicao) {
         this.cor = cor;
-        this.posicaoX = posicaoX;
-        this.posicaoY = posicaoY;
+        this.posicao = posicao;
     }
 
     public String getCor() {
         return cor;
     }
 
-    public int getPosicaoX() {
-        return posicaoX;
+    public int getPosicao() {
+        return posicao;
     }
 
-    public void setPosicaoX(int posicaoX) {
-        this.posicaoX = posicaoX;
+    public void setPosicao(int posicao) {
+        this.posicao = posicao;
     }
 
-    public int getPosicaoY() {
-        return posicaoY;
-    }
-
-    public void setPosicaoY() {
-        this.posicaoY = posicaoX;
-    }
-
-    abstract public ArrayList<ArrayList<Integer>> getMovimento(int posicaoX, int posicaoY);
+    abstract public ArrayList<Integer> getMovimento(int posicao);
 }
