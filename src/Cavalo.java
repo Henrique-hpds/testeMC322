746d447df26f48a02ddec36a5bbf0c87a4a047bb
Vinicius Patriarca <v260731@dac.unicamp.br>
2023-06-22 19:31:15 -0300

    Coordenadas cavalo

--- src/Cavalo.java
+++ src/Cavalo.java
@@ -2,10 +2,11 @@ import java.util.ArrayList;
 
 public class Cavalo extends Peca{
 
-    private final static int[] MOVIMENTO_PADRAO = {-17, -15, -10, -6, 6, 10, 15, 17};
+    private final static  ArrayList<Integer> MOVIMENTO_PADRAO = {-17, -15, -10, -6, 6, 10, 15, 17};
 
     @Override
     public ArrayList<Integer> getPossiveisMovimentos() {
         List <Integer> movimentos = new ArrayList<>();
+        return ArrayList<Integer> MOVIMENTO_PADRAO = {getPosicao() -17, getPosicao() - 15,  getPosicao() - 10, getPosicao() -6, getPosicao() + 6, getPosicao() + 10, getPosicao() + 15, getPosicao() + 17};
     }
 }
