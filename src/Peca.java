768e837181df28f8da463cfd6aecb4fd159bc6bc
Victor Satoh <47898144+vsatoh@users.noreply.github.com>
2023-06-22 16:26:53 -0300

    adicionei algumas variaveis e fiz os gets e sets

--- src/Peca.java
+++ src/Peca.java
@@ -1,6 +1,30 @@
 public class Peca {
-    public String cor;
+    public final String cor;
+    public int posicaoX, posicaoY;
 
+    public Peca(String cor, int posicaoX, int posicaoY) {
+        this.cor = cor;
+        this.posicaoX = posicaoX;
+        this.posicaoY = posicaoY;
+    }
 
-    
+    public String getCor() {
+        return cor;
+    }
+
+    public int getPosicaoX() {
+        return posicaoX;
+    }
+
+    public void setPosicaoX(int posicaoX) {
+        this.posicaoX = posicaoX;
+    }
+
+    public int getPosicaoY() {
+        return posicaoY;
+    }
+
+    public void setPosicaoY() {
+        this.posicaoY = posicaoX;
+    }
 }
