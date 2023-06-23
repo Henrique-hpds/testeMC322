7a7e650ca9a9691d5559fc266531ca41161c4f86
Lima-Almeida <m242827@dac.unicamp.br>
2023-06-23 15:48:44 -0300

    Mudança nome Manager

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 public class Main{  
     public static void main(String[] args){  
 
-		Maneger jogo = new Maneger();		
+		Manager jogo = new Manager();		
 
 		jogo.iniciarTabuleiro();
 		
