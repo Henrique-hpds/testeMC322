54cba050b71478eb8441999f14c4cfadcd678d4d
Victor Satoh <47898144+vsatoh@users.noreply.github.com>
2023-06-23 17:08:35 -0300

    funcao de comer peca

--- src/Peca.java
+++ src/Peca.java
@@ -38,4 +38,10 @@ public class Peca {
 
         return movimentos;
     }
+
+    public static Peca comerPeca(Casa casa) {
+        Peca peca = casa.getPeca();
+        casa.setPeca(null);
+        return peca;
+    }
 }
