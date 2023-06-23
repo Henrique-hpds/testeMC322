9dcfb5aabbc59153a130c0202754e38ad50a0bfe
Victor Satoh <47898144+vsatoh@users.noreply.github.com>
2023-06-23 15:56:24 -0300

    funcao de ver se a casa esta vazia

--- src/Validacao.java
+++ src/Validacao.java
@@ -10,7 +10,18 @@ public class Validacao {
         return true;
     }
 
-    // public static boolean casaVazia(Tabuleiro tabuleiro, Coordenada coord) {
-    //     if(tabuleiro.getCasa(coord.x(),coord.y()))
-    // }
+    public static boolean casaVazia(Tabuleiro tabuleiro, Coordenada coord, Peca peca) {
+        if(tabuleiro.getCasa(coord.x(),coord.y()).getPeca() == null) {
+            return true;
+        }
+        else if(tabuleiro.getCasa(coord.x(),coord.y()).getPeca().getCor() == peca.getCor()) {
+            //verifica se na coordenada da casa contem alguma peca de mesma cor
+            return false;
+        } 
+        else if(tabuleiro.getCasa(coord.x(),coord.y()).getPeca().getCor() != peca.getCor()) {
+            //chamar funcao de comer outra peca
+            return true;
+        }
+        return false;
+    }
 }
