8fe3367126bf2529b48a6ffe1da884d7c4aa792e
Lima-Almeida <m242827@dac.unicamp.br>
2023-06-25 17:52:32 -0300

    Movimento peao (inicio) detalhes

--- src/Peao.java
+++ src/Peao.java
@@ -12,7 +12,16 @@ public class Peao extends Peca{
     private final static Coordenada[] MOVIMENTO_PADRAO = {new Coordenada(0, 1), new Coordenada(0, 2), new Coordenada(-1, 1), new Coordenada(1, 1), new Coordenada(0, -1), new Coordenada(0, -2), new Coordenada(-1, -1), new Coordenada(1, -1)};
 
     //Atenção para as condições especiais de primeira jogada, usar a boolean "primeiraJogada"
-    public ArrayList<Coordenada> getPossiveisMovimentos(Tabuleiro tabuleiro){
-        return super.getPossiveisMovimentos(MOVIMENTO_PADRAO, tabuleiro);
+    public ArrayList<Coordenada> getPossiveisMovimentos(Tabuleiro tabuleiro) {
+        ArrayList <Coordenada> movimentosPossiveis = new ArrayList<Coordenada>();
+        ArrayList <Coordenada> movimentosLegais = new ArrayList<Coordenada>();
+        if(primeiraJogada) {
+            //Aplicar aqui condições para primeira jogada (andar duas casas)
+        }
+        else {
+            //Restante
+        }
+        return movimentosLegais;
+        //return super.getPossiveisMovimentos(MOVIMENTO_PADRAO, tabuleiro);
     }
 }
