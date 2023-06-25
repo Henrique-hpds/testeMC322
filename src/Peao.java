a38bd5c42639036f95f6bc3f0d0ac573bf2a26c3
Lima-Almeida <m242827@dac.unicamp.br>
2023-06-25 17:48:41 -0300

    Pequenas alteraçoes logica peao

--- src/Peao.java
+++ src/Peao.java
@@ -1,13 +1,17 @@
 import java.util.ArrayList;
 
 public class Peao extends Peca{
+
+    boolean primeiraJogada;
     
     public Peao(String cor, Coordenada posicao){
         super(cor, posicao, 1, "peao");
+        this.primeiraJogada = true;
     }
 
     private final static Coordenada[] MOVIMENTO_PADRAO = {new Coordenada(0, 1), new Coordenada(0, 2), new Coordenada(-1, 1), new Coordenada(1, 1), new Coordenada(0, -1), new Coordenada(0, -2), new Coordenada(-1, -1), new Coordenada(1, -1)};
 
+    //Atenção para as condições especiais de primeira jogada, usar a boolean "primeiraJogada"
     public ArrayList<Coordenada> getPossiveisMovimentos(Tabuleiro tabuleiro){
         return super.getPossiveisMovimentos(MOVIMENTO_PADRAO, tabuleiro);
     }
