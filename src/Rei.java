d72323afefaea4b20bf73e36a8fda20eb7ae0857
Lima-Almeida <m242827@dac.unicamp.br>
2023-06-25 17:01:57 -0300

    Movimentacao rei att

--- src/Rei.java
+++ src/Rei.java
@@ -9,6 +9,53 @@ public class Rei extends Peca{
 
     
     public ArrayList<Coordenada> getPossiveisMovimentos(Tabuleiro tabuleiro){
-        return super.getPossiveisMovimentos(MOVIMENTO_PADRAO, tabuleiro);
+        ArrayList <Coordenada> movimentosPossiveis = new ArrayList<Coordenada>();
+        ArrayList <Coordenada> movimentosLegais = new ArrayList<Coordenada>();
+        for(int i = -1; i <= 1; i++) {
+            Coordenada coord = new Coordenada(i, i);
+            Coordenada soma = getPosicao().soma(coord);
+            if(!tabuleiro.getCasa(soma.x(), soma.y()).estaOcupado() || !tabuleiro.getCasa(soma.x(), soma.y()).getCor().equals(getCor())) {
+                movimentosPossiveis.add(soma);
+            }
+            else {
+                break;
+            }
+        }
+        for(int i = -1; i <= 1; i++) {
+            Coordenada coord = new Coordenada(i, i);
+            Coordenada soma = getPosicao().soma(coord);
+            if(!tabuleiro.getCasa(-soma.x(), soma.y()).estaOcupado() || !tabuleiro.getCasa(-soma.x(), soma.y()).getCor().equals(getCor())) {
+                movimentosPossiveis.add(coord);
+            }
+            else {
+                break;
+            }
+        }
+        for(int i = -1; i <= 1; i++) {
+            Coordenada coord = new Coordenada(i, 0);
+            Coordenada soma = getPosicao().soma(coord);
+            if(!tabuleiro.getCasa(soma.x(), soma.y()).estaOcupado() || !tabuleiro.getCasa(soma.x(), soma.y()).getCor().equals(getCor())) {
+                movimentosPossiveis.add(soma);
+            }
+            else {
+                break;
+            }
+        }
+        for(int i = -1; i <= 1; i++) {
+            Coordenada coord = new Coordenada(0, i);
+            Coordenada soma = getPosicao().soma(coord);
+            if(!tabuleiro.getCasa(soma.x(), soma.y()).estaOcupado() || !tabuleiro.getCasa(soma.x(), soma.y()).getCor().equals(getCor())) {
+                movimentosPossiveis.add(soma);
+            }
+            else {
+                break;
+            }
+        }
+        for (Coordenada coord: movimentosPossiveis){
+            Coordenada soma = getPosicao().soma(coord);
+            if (Validacao.coordenadaValida(soma))
+                movimentosLegais.add(soma);
+        }
+        return movimentosLegais;
     }
 }
