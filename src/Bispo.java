87125ede4b9c472bddc5dc4d7503f5dcc36e802b
Victor Satoh <47898144+vsatoh@users.noreply.github.com>
2023-06-25 13:57:28 -0300

    lista movimentos bisbo

--- src/Bispo.java
+++ src/Bispo.java
@@ -9,6 +9,31 @@ public class Bispo extends Peca{
     private final static Coordenada[] MOVIMENTO_PADRAO = {new Coordenada(-7, -7), new Coordenada(-6, -6), new Coordenada(-5, -5), new Coordenada(-4, -4), new Coordenada(-3, -3), new Coordenada(-2, -2), new Coordenada(-1, -1), new Coordenada(1, 1), new Coordenada(2, 2), new Coordenada(3, 3), new Coordenada(4, 4), new Coordenada(5, 5), new Coordenada(6, 6), new Coordenada(7, 7), new Coordenada(7, -7), new Coordenada(6, -6), new Coordenada(5, -5), new Coordenada(4, -4), new Coordenada(3, -3), new Coordenada(2, -2), new Coordenada(1, -1), new Coordenada(1, -1), new Coordenada(2, -2), new Coordenada(3, -3), new Coordenada(4, -4), new Coordenada(5, -5), new Coordenada(6, -6), new Coordenada(7, -7)};
 
     public ArrayList<Coordenada> getPossiveisMovimentos(Tabuleiro tabuleiro){
-        return super.getPossiveisMovimentos(MOVIMENTO_PADRAO, tabuleiro);
+        ArrayList <Coordenada> movimentosPossiveis = new ArrayList<Coordenada>();
+        ArrayList <Coordenada> movimentosLegais = new ArrayList<Coordenada>();
+        for(int i = -7; i <= 7; i++) {
+            if(!tabuleiro.getCasa(i, i).estaOcupado() || !tabuleiro.getCasa(i, i).getCor().equals(getCor())) {
+                Coordenada coord = new Coordenada(i, i);
+                movimentosPossiveis.add(coord);
+            }
+            else {
+                break;
+            }
+        }
+        for(int i = -7; i <= 7; i++) {
+            if(!tabuleiro.getCasa(-i, i).estaOcupado() || !tabuleiro.getCasa(-i, i).getCor().equals(getCor())) {
+                Coordenada coord = new Coordenada(-i, i);
+                movimentosPossiveis.add(coord);
+            }
+            else {
+                break;
+            }
+        }
+        for (Coordenada coord: movimentosPossiveis){
+            Coordenada soma = getPosicao().soma(coord);
+            if (Validacao.coordenadaValida(soma))
+                movimentosLegais.add(soma);
+        }
+        return movimentosLegais;
     }
 }
